91a6822160600dafde72d8ed0136b2ed6d4bc7f5
lno001 <lno001@hotmail.com>
2026-04-06 17:49:22 +0900

    ai의 도움을 받는 rpg구조 만들기 시작

---
+++ Study_02_SimpleRpg/src/com/kh/simplerpg/character/model/Character.java
@@ -0,0 +1,16 @@
+package com.kh.simplerpg.character.model;
+
+public class Character {
+	private int saveNum;
+	private String characterName;
+	private Job job;
+	private CharacterStat stats;
+	private int currentHp;
+	private int currentMp;
+	
+	
+	
+	
+	
+	
+}
